ee011c0ef64dca6d0c3bb0ddfee1c892d16e76d7
Rupeshhh <jenarupesh16@gmail.com>
2026-07-03 14:08:54 +0530

    Time: 0 ms (100%), Space: 44.1 MB (11.11%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,24 +1,29 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int low = 0 , high = nums.length - 1;
-        while(low <= high){
-            int mid = (low + high) / 2;
+        int low = 0;
+        int high = nums.length - 1;
+
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+
             if(nums[mid] == target) return mid;
+            // then we'll check in which half the target lies whether in low .. mid half or mid+1 .. high half accordingly we'll move the low and high pointers
+            
+            // searching in part-1
+            else if(nums[low] <= nums[mid]) {
+                // move the low and high if target lies in the left half
+                if(nums[low] <= target && target <= nums[mid]) high = mid - 1;
+                else low = mid + 1;
+            } 
 
-            if(nums[low] <= nums[mid]){
-                if(nums[low] <= target && target <= nums[mid])
-                    high = mid - 1;
-                else
-                    low = mid + 1; 
-            }
-
+            // searching in part-2
             else {
-                if(nums[mid] <= target && target <= nums[high])
-                    low = mid + 1;
-                else 
-                    high = mid - 1;
+                // move the low and high if target lies in the right half
+                if(nums[mid+1] <= target && target <= nums[high]) low = mid + 1;
+                else high = mid - 1;
             }
         }
-        return -1;
+
+        return -1; 
     }
 }
