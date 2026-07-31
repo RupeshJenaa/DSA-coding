e7d3ecd16ebfc9074fad6bed947ad91b6a54cbba
Rupeshhh <jenarupesh16@gmail.com>
2026-07-31 12:52:55 +0530

    Time: 10 ms (66.76%), Space: 48.1 MB (32.08%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -1,48 +1,68 @@
 class Solution {
     public int minimumPushes(String word) {
+        // int count = 0;
+        // HashMap<Character,Integer> map = new HashMap<>();
+        // for(int i=0; i<word.length(); i++) {
+        //     char ch = word.charAt(i);
+        //     map.put(ch, map.getOrDefault(ch, 0) + 1);
+        // }
+        // List<Integer> freqs = new ArrayList<>(map.values());
+        // freqs.sort(Collections.reverseOrder());
+        // int len = freqs.size();
+
+        // // 1st condition - len is between 1-8 (cost_push = 1)
+        // if(len <= 8) {
+        //     for(int ele: freqs) {
+        //         count += ele;
+        //     }
+        // }
+
+        // // 2nd condition - len is between 9-16 (cost_push = 2)
+        // // so here - count += (sum of first 8) + (sum of rem)*2
+        // else if(len > 8 && len <= 16) {
+        //     for(int i = 0; i < len; i++) {
+        //         if(i <= 7) count += freqs.get(i);
+        //         else count += freqs.get(i) * 2;
+        //     }
+        // }
+
+        // // 3rd condition - len is between 17-24
+        // else if(len > 16 && len <= 24) {
+        //     for(int i = 0; i < len; i++) {
+        //         if(i <= 7) count += freqs.get(i);
+        //         else if(i <= 15) count += freqs.get(i) * 2;
+        //         else count += freqs.get(i) * 3;
+        //     }
+        // }
+
+        // //4th condition - len is 25-26
+        // else {
+        //     for(int i = 0; i < len; i++) {
+        //         if(i <= 7) count += freqs.get(i);
+        //         else if(i <= 15) count += freqs.get(i) * 2;
+        //         else if(i <= 23) count += freqs.get(i) * 3;
+        //         else count += freqs.get(i) * 4;
+        //     }
+        // }
+
+        // return count;
+
         int count = 0;
-        HashMap<Character,Integer> map = new HashMap<>();
+        int[] freq = new int[26];
+        Arrays.fill(freq, 0);
         for(int i=0; i<word.length(); i++) {
             char ch = word.charAt(i);
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
+            freq[ch - 'a']++;
         }
-        List<Integer> freqs = new ArrayList<>(map.values());
-        freqs.sort(Collections.reverseOrder());
-        int len = freqs.size();
+        Arrays.sort(freq); // ascending order
+        int assigned_keypad_pos = 0; // for every 8 characters same push cost
 
-        // 1st condition - len is between 1-8 (cost_push = 1)
-        if(len <= 8) {
-            for(int ele: freqs) {
-                count += ele;
-            }
-        }
+        for(int i = 25; i >= 0; i--) {
+            if(freq[i] == 0) break; // if once 0 is encountered then further elements have also 0 because sorted na 
 
-        // 2nd condition - len is between 9-16 (cost_push = 2)
-        // so here - count += (sum of first 8) + (sum of rem)*2
-        else if(len > 8 && len <= 16) {
-            for(int i = 0; i < len; i++) {
-                if(i <= 7) count += freqs.get(i);
-                else count += freqs.get(i) * 2;
-            }
-        }
-
-        // 3rd condition - len is between 17-24
-        else if(len > 16 && len <= 24) {
-            for(int i = 0; i < len; i++) {
-                if(i <= 7) count += freqs.get(i);
-                else if(i <= 15) count += freqs.get(i) * 2;
-                else count += freqs.get(i) * 3;
-            }
-        }
-
-        //4th condition - len is 25-26
-        else {
-            for(int i = 0; i < len; i++) {
-                if(i <= 7) count += freqs.get(i);
-                else if(i <= 15) count += freqs.get(i) * 2;
-                else if(i <= 23) count += freqs.get(i) * 3;
-                else count += freqs.get(i) * 4;
-            }
+            count += freq[i] * ((assigned_keypad_pos / 8) + 1);
+            // / 8 -> gives keypad_pos and + 1 is to give push_cost of that respective keypad_pos
+            assigned_keypad_pos++; // inc. the keypad_pos everytime
         }
 
         return count;
